1150b3fd788debf7db781029c364e1d80fd31095
ishoborabyose <ishoborabyosebeatrice@gmail.com>
2023-03-31 15:33:53 +0200

    Feat: Context-todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,73 @@
+import { TodoContext } from "../features/NoteContext";
+import { useContext } from "react";
+import { MdDelete, MdEdit, MdSave, MdCancel } from "react-icons/md";
+
+const Todo = () => {
+  const {
+    todos,
+    handleTodoDelete,
+    handleTodoEdit,
+    handleTodoStartEdit,
+    handleTodoCancelEdit,
+    handleTodoComplete,
+  } = useContext(TodoContext);
+  return (
+    <div className="max-w-3xl mx-auto py-11 px-4">
+      {todos.map((todo, index) => {
+        return (
+          <div className="pt-8 border-b-2">
+            {todo.isEditing ? (
+              <div className="flex text-center">
+                <input
+                  className="bg-white w-full px-4 py-5 rounded-full outline-none placeholder:text-base placeholder:text-gray-800"
+                  type="text"
+                  defaultValue={todo.text}
+                  autoFocus
+                  onBlur={(e) => handleTodoEdit(index, e.target.value)}
+                  onKeyDown={(e) => {
+                    e.key === "Escape" ? handleTodoCancelEdit(index) : "";
+                  }}
+                />
+                <button
+                  className="cursor-pointer"
+                  onClick={() => handleTodoEdit(index)}
+                >
+                  <MdSave className="w-7 h-10 text-[#61c7c6]" />
+                </button>
+                <button
+                  className="ml-2 cursor-pointer"
+                  onClick={() => handleTodoCancelEdit(index)}
+                >
+                  <MdCancel className="ml-2 h-10 w-7 text-[#ff4d4d]" />
+                </button>
+              </div>
+            ) : (
+              <div className="flex items-center">
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  onClick={() => handleTodoComplete(index)}
+                />
+                <p
+                  className={`flex-1 ml-2 ${todo.completed && "line-through"}`}
+                >
+                  {todo.text}
+                </p>
+                <div>
+                  <button onClick={() => handleTodoStartEdit(index)}>
+                    <MdEdit className="text-[#61c7c6] w-7 h-10" />
+                  </button>
+                  <button onClick={() => handleTodoDelete(index)}>
+                    <MdDelete className="text-[#ff4d4d] w-7 h-10" />
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Todo;
